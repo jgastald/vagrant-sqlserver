3806db072ce47d930b416f7081179c44c2e19e5c
Jorge Gastaldi <890919+jgastald@users.noreply.github.com>
2019-01-23 01:47:44 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,35 +2,51 @@
 # Part of Vagrant virtual environments for SQL Server 2017 on Ubuntu Linux
 #
 # Setup environment configuration
+
+
 export ACCEPT_EULA="Y"
-export MSSQL_PID="Developer"
+export MSSQL_PID="Express"
 export MSSQL_SA_PASSWORD="Password123"
 export DEBIAN_FRONTEND="noninteractive"
-# Ensure hostname is recognised
-sudo sed -i "s/^127\.0\.0\.1.*/127.0.0.1 localhost $HOSTNAME/g" /etc/hosts
-# Install pre-requisites
+
+# # Ensure hostname is recognised
+# sudo sed -i "s/^127\.0\.0\.1.*/127.0.0.1 localhost $HOSTNAME/g" /etc/hosts
+
+# # Install pre-requisites
 sudo apt-get -y -q update
-## Ubuntu 16.04 does not deliver add-apt-repository by default
-sudo apt-get -y -q install curl software-properties-common
-# Pre-installation
-curl -s -S --retry 3 https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-## Repository Microsoft SQL Server
-sudo add-apt-repository "$(curl -s -S --retry 3 https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2017.list)"
-## Repository SQL Server command-line tools
-sudo add-apt-repository "$(curl -s -S --retry 3 https://packages.microsoft.com/config/ubuntu/16.04/prod.list)"
+
+# ## Ubuntu 16.04 does not deliver add-apt-repository by default
+# sudo apt-get -y -q install curl software-properties-common
+
+# # Pre-installation
+# curl -s -S --retry 3 https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+# ## Repository Microsoft SQL Server
+# sudo add-apt-repository "$(curl -s -S --retry 3 https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2017.list)"
+ sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2017.list)"
+
+# ## Repository SQL Server command-line tools
+# sudo add-apt-repository "$(curl -s -S --retry 3 https://packages.microsoft.com/config/ubuntu/16.04/prod.list)"
+curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+# echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/16.04/prod xenial main" | sudo tee /etc/apt/sources.list.d/mssql.lis
+
 sudo apt-get -y -q update
-sudo -E bash -c 'apt-get -y -q install mssql-server'
-sudo -E bash -c 'apt-get -y -q install mssql-tools'
-# Clean up
+# sudo -E bash -c 'apt-get -y -q install mssql-server'
+# sudo -E bash -c 'apt-get -y -q install mssql-tools'
+sudo apt-get install -y -q mssql-server
+sudo apt-get install -y -q mssql-tools unixodbc-dev
+# # Clean up
 sudo apt-get -y -q autoremove
 sudo apt-get -y -q clean
-# Post-installation
+
+# # Post-installation
 echo "SQLServer: running /opt/mssql/bin/mssql-conf -n setup"
 echo "SQLServer: MSSQL_PID=$MSSQL_PID"
 echo "SQLServer: MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD"
 sudo -E bash -c '/opt/mssql/bin/mssql-conf -n setup'
 sudo /opt/mssql/bin/mssql-conf set telemetry.customerfeedback false
-echo "SQLServer: restarting"
+# echo "SQLServer: restarting"
 sudo systemctl stop mssql-server
 sudo systemctl start mssql-server
 sudo systemctl status mssql-server
